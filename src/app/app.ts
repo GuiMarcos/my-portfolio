897b5c46345213930c9fc0c6dc11a4f8222aee17
Guilherme Marcos <estag.guilhermea@utfpr.edu.br>
2026-03-09 12:29:23 -0300

    feat: add Portfolio component with layout and styles; update app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from '@components/header/header';
 import { Hero } from '@components/hero/hero';
 import { About } from '@components/about/about';
+import { Portfolio } from "@components/portfolio/portfolio";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero, About],
+  imports: [Header, Hero, About, Portfolio],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
